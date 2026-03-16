ff64463f6148938a460a93122629882045e570cc
seveibar <seveibar@gmail.com>
2026-03-16 13:30:02 -0700

    some structure

---
+++ tests/fixtures/renderCircuit.ts
@@ -0,0 +1,18 @@
+import { RootCircuit } from "tscircuit";
+import { getPlatformConfig } from "@tscircuit/eval";
+
+export const renderCircuit = async (tsxElement: React.ReactElement) => {
+	const circuit = new RootCircuit({
+		platform: {
+			...getPlatformConfig(),
+			partsEngineDisabled: true,
+			routingDisabled: true,
+		},
+	});
+
+	circuit.add(tsxElement);
+
+	await circuit.renderUntilSettled();
+
+	return circuit.getCircuitJson();
+};
